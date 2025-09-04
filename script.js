cc95d75186e4a8c5b119dabd953b86a9ca6aad47
Carlos Jiménez <103526659+charlimax@users.noreply.github.com>
2025-09-04 18:41:15 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// SCRIPT MENU HAMBURGUESA
+const hamburger = document.getElementById('hamburger');
+const menu = document.getElementById('menu');
+
+hamburger.addEventListener('click', () => {
+  menu.classList.toggle('show');
+});
